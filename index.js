17996f201a898b42ed8723b1b3e2b170cca98f1c
Hal <hgrforrest@gmail.com>
2021-12-09 15:26:28 -0500

    jsoning with await vs then :ring:

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 const aFuncToAsync = () => {
 	return new Promise(resolve => {
 		setTimeout(() => resolve('I did it!', 3000))
@@ -24,3 +26,47 @@ aPromiseFunc().then(res => {
 	console.log(res);
 })
 console.log('after');
+
+const getFile = (filename) => {
+	return new Promise((resolve, reject) => {
+		fs.readFile(filename, (err, data) => {
+			if (err) {
+				console.log(err)
+				reject(err)
+				return
+			}
+
+			// console.log("data: ", data)
+			resolve(data)
+		})
+	})
+}
+
+const getFirstUserData = () => {
+	getFile('./users.json')
+		.then(res => JSON.parse(res))
+		.then(res => {
+			return res[0].name;
+		})
+		.then(userName => {
+			return getFile(`./users/${userName}.json`)
+		})
+		.then(res => JSON.parse(res))
+		.then(res => {
+			console.log(res.description);
+		})
+}
+
+getFirstUserData()
+
+const getSecondUserDataAsync = async () => {
+	const response = await getFile('./users.json')
+	const users = await JSON.parse(response)
+	const user = users[1]
+	const userResponse = await getFile(`./users/${user.name}.json`)
+	const userData = await JSON.parse(userResponse)
+	console.log("radnom: ", userData.description);
+	return userData
+}
+
+getSecondUserDataAsync()
